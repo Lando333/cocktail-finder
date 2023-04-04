55fb46218c271408aff01bbaabdbeb239aecbafb
Ryan Rojas <landoslair@gmail.com>
2023-04-04 10:00:42 -0400

    added conditional functionality to radio buttons

--- index.js
+++ index.js
@@ -1,15 +1,25 @@
-const url = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s='
+const cocktailUrl = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s='
+const ingredientUrl = 'https://www.thecocktaildb.com/api/json/v1/1/filter.php?i='
 const submitForm = document.getElementById('search-form')
 const resultsList = document.getElementById('results-list')
+let ingredient = document.getElementById('')
 
 submitForm.addEventListener('submit', (e) => {
     e.preventDefault()
     const searchWord = e.target.search.value
-    fetchCocktails(searchWord)
+    if (e.target.ingredient.checked)
+        fetchIngredient(searchWord)
+    else if (e.target.cocktail.checked)
+        fetchCocktails(searchWord)
     submitForm.reset()
 })
 function fetchCocktails(searchWord) {
-    fetch(url + searchWord)
+    fetch(cocktailUrl + searchWord)
+        .then(r => r.json())
+        .then(cocktailData => renderSearchList(cocktailData))
+}
+function fetchIngredient(searchWord) {
+    fetch(ingredientUrl + searchWord)
         .then(r => r.json())
         .then(cocktailData => renderSearchList(cocktailData))
 }
@@ -22,4 +32,6 @@ function renderDrink(drink) {
     const li = document.createElement('li')
     li.innerText = drink.strDrink
     resultsList.appendChild(li)
+
+    
 }
