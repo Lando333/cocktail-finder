02950850d1235dce1e1c7e8f31d5af4838dfc6b6
Ryan Rojas <landoslair@gmail.com>
2023-04-04 14:07:22 -0400

    updated functionality/added recipe display

--- index.js
+++ index.js
@@ -1,26 +1,32 @@
 const baseUrl = 'https://www.thecocktaildb.com/api/json/v1/1/'
-const cocktailUrl = 'search.php?s='
-const ingredientUrl = 'filter.php?i='
+const ingredientUrl = baseUrl + 'filter.php?i='
+const cocktailUrl = baseUrl + 'search.php?s='
 const submitForm = document.getElementById('search-form')
 const resultsList = document.getElementById('results-list')
-let ingredient = document.getElementById('')
+
+const recipeDiv = document.getElementById('text-box')
+const imageDiv = document.getElementById('image-box')
+const drinkName = document.createElement('li')
 
 submitForm.addEventListener('submit', (e) => {
     e.preventDefault()
-    const searchWord = e.target.search.value
-    if (document.getElementById('element_1'))
+    resultsList.innerHTML = ""
+    const searchWord = encodeURI(e.target.search.value)
+    if (document.getElementById('element_1').checked)
         fetchIngredient(searchWord)
-    else if (document.getElementById('element_2'))
+    else if (document.getElementById('element_2').checked)
         fetchCocktails(searchWord)
     submitForm.reset()
+    
 })
-function fetchCocktails(searchWord) {
-    fetch(baseUrl + cocktailUrl + searchWord)
+
+function fetchIngredient(searchWord) {
+    fetch(ingredientUrl + searchWord)
         .then(r => r.json())
         .then(cocktailData => renderSearchList(cocktailData))
 }
-function fetchIngredient(searchWord) {
-    fetch(baseUrl + ingredientUrl + searchWord)
+function fetchCocktails(searchWord) {
+    fetch(cocktailUrl + searchWord)
         .then(r => r.json())
         .then(cocktailData => renderSearchList(cocktailData))
 }
@@ -29,8 +35,24 @@ function renderSearchList(list) {
     list.drinks.forEach(element => renderDrink(element));
 }
 function renderDrink(drink) {
-    console.log(drink)
     const li = document.createElement('li')
     li.innerText = drink.strDrink
     resultsList.appendChild(li)
+    li.addEventListener('click', () => {
+        recipeDiv.innerHTML = ""
+        imageDiv.innerHTML = ""
+        populateRecipe(drink)
+    })
+}
+
+function populateRecipe(recipe) {
+    
+    drinkName.innerText = recipe.strDrink
+    recipeDiv.appendChild(drinkName)
+
+    const drinkImage = document.createElement('img')
+    drinkImage.src = recipe.strDrinkThumb
+    imageDiv.appendChild(drinkImage)
+
+    console.log(recipe)
 }
